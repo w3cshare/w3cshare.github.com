a37c27691be3877937dca9828e81f76bc4e5749e
wangwei <wwdqq7@qq.com>
2025-04-23 16:46:18 +0800

    chore: 生成 .gitignore 文件并添加 Prettier 配置
    - 在各个目录下生成 .gitignore 文件，忽略 dist、node_modules 和 lib 目录
    - 添加 .prettierrc 文件，配置代码格式化规则
    - 更新 gitignore.js 脚本，用于自动生成 .gitignore 文件

--- _scripts/gitignore.js
+++ _scripts/gitignore.js
@@ -1,23 +1,31 @@
-const fs = require('fs');
-const { globSync } = require('glob');
-const path = require('path');
+/*
+ * @Author: wangwei wwdqq7@qq.com
+ * @Date: 2025-04-14 22:39:19
+ * @LastEditors: wangwei wwdqq7@qq.com
+ * @LastEditTime: 2025-04-23 16:44:39
+ * @FilePath: /FullStack/_scripts/gitignore.js
+ * @Description: gitignore文件生成脚本
+ */
+const fs = require('fs')
+const { globSync } = require('glob')
+const path = require('path')
 
 try {
   const directories = globSync('./*/*/', {
     ignore: ['node_modules/**', 'utils/**', 'docs/**'],
     absolute: true,
-  });
+  })
 
   directories.forEach(dir => {
     // 判断文件是否存在
-    const gitignorePath = path.join(dir, '.gitignore');
-    const isFileExist = fs.existsSync(gitignorePath);
+    const gitignorePath = path.join(dir, '.gitignore')
+    const isFileExist = fs.existsSync(gitignorePath)
     if (!isFileExist) {
-      console.log('🚀 ~ file: gitignore.js:20 ~ dir:', dir);
-      fs.writeFileSync(gitignorePath, `dist\n\rnode_modules`, 'utf-8');
+      console.log('🚀 ~ file: gitignore.js:20 ~ dir:', dir)
+      fs.writeFileSync(gitignorePath, 'node_modules\ndist\nlib\n\r', 'utf-8')
     }
-  });
+  })
 } catch (error) {
-  console.error('目录遍历失败：', error.message);
-  process.exitCode = 1;
+  console.error('目录遍历失败：', error.message)
+  process.exitCode = 1
 }
