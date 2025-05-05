87c17f1ddfb4d78a74d7f6faf423702c650510a5
wangwei <wwdqq7@qq.com>
2025-05-05 18:41:04 +0800

    feat(theme): 实现页面全宽布局并优化样式
    - 新增自定义 CSS 变量，覆盖 VitePress 默认布局限制
    - 调整内容区域、侧边栏等元素的宽度和间距
    - 优化表格、图片等元素的显示效果
    - 增加媒体查询以适配移动设备
    - 删除部分冗余的样式文件
    - 更新项目中多个子目录的 ESLint 和 Prettier 配置

--- .vitepress/theme/index.mts
+++ .vitepress/theme/index.mts
@@ -2,14 +2,15 @@
  * @Author: wangwei wwdqq7@qq.com
  * @Date: 2025-04-14 12:27:56
  * @LastEditors: wangwei wwdqq7@qq.com
- * @LastEditTime: 2025-04-21 15:53:55
+ * @LastEditTime: 2025-05-05 18:06:39
  * @FilePath: /FullStack/.vitepress/theme/index.ts
  * @Description:
  */
-import DefaultTheme from 'vitepress/theme';
-import type { Theme } from 'vitepress';
-import './style.css';
+import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
+// import './style.css';
+// import './custom.css';
 
 export default {
   extends: DefaultTheme,
-} as Theme;
+} as Theme
--- _scripts/prettier.js
+++ _scripts/prettier.js
@@ -2,21 +2,45 @@
  * @Author: wangwei wwdqq7@qq.com
  * @Date: 2025-05-05 13:04:43
  * @LastEditors: wangwei wwdqq7@qq.com
- * @LastEditTime: 2025-05-05 17:30:13
+ * @LastEditTime: 2025-05-05 18:28:40
  * @FilePath: /FullStack/_scripts/prettier.js
- * @Description:
+ * @Description: 获取项目中的二级目录（子项目目录）路径
  */
 const fs = require('fs')
 const { globSync } = require('glob')
 const path = require('path')
 
-const PROJECT_STRUCTURE_PATHS = globSync('**/PROJECT_STRUCTURE.md', {
+// const PROJECT_STRUCTURE_PATHS = globSync('**/PROJECT_STRUCTURE.md', {
+//   cwd: process.cwd(),
+//   ignore: ['**/node_modules/**', '**/.git/**', '**/docs/**', 'docs/**'],
+// })
+
+// PROJECT_STRUCTURE_PATHS.forEach(filePath => {
+//   fs.unlinkSync(filePath)
+// })
+
+// console.log(PROJECT_STRUCTURE_PATHS)
+
+/**
+ * 获取二级目录（子项目目录）路径
+ * 根据项目结构，直接列出主要的子项目目录类型
+ */
+const subProjectPaths = globSync('{apps,lib-*,micro-*,libs}/*/', {
+  // const subProjectPaths = globSync('{apps-java,apps-native,apps-python}/*/', {
   cwd: process.cwd(),
   ignore: ['**/node_modules/**', '**/.git/**', '**/docs/**', 'docs/**'],
+  onlyDirectories: true,
 })
-
-PROJECT_STRUCTURE_PATHS.forEach(filePath => {
-  fs.unlinkSync(filePath)
+const PRETTIERRC_CJS_TEMPLATE = `
+module.exports = {
+  plugins: [require('prettier-plugin-smart')],
+  ...require('prettier-plugin-smart').defaultOptions,
+}
+`
+subProjectPaths.forEach(projectDir => {
+  const prettierrcCjsPath = path.join(process.cwd(), projectDir, '.prettierrc.cjs')
+  const prettierrcPath = path.join(process.cwd(), projectDir, '.prettierrc')
+  fs.existsSync(prettierrcPath) && fs.unlinkSync(prettierrcPath)
+  fs.writeFileSync(prettierrcCjsPath, PRETTIERRC_CJS_TEMPLATE)
 })
-
-console.log(PROJECT_STRUCTURE_PATHS)
+console.log('🚀 ~ file: prettier.js:40 ~ subProjectPaths:', subProjectPaths)
