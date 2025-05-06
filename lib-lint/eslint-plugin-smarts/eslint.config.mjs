941fef466f5ce227ff8e03fd9cbd217ff7e591e1
wangwei <wwdqq7@qq.com>
2025-05-06 12:05:52 +0800

    refactor(lint): 重构 commitlint 配置和文档
    - 更新了 commitlint 配置文件路径和内容
    - 移除了多余的文档文件
    - 修改了包.json 文件中的相关配置
    - 重命名了相关文件以提高一致性

--- lib-lint/eslint-plugin-smarts/eslint.config.mjs
+++ lib-lint/eslint-plugin-smarts/eslint.config.mjs
@@ -1,37 +1,7 @@
-/*
- * @Author: wangwei wwdqq7@qq.com
- * @Date: 2025-04-21 16:08:29
- * @LastEditors: wangwei wwdqq7@qq.com
- * @LastEditTime: 2025-04-28 02:02:12
- * @FilePath: /FullStack/micro-service/grpc-gateway/eslint.config.mjs
- * @Description: eslint配置
- */
 
-// eslint.config.mjs
 import eslintPlugin from 'eslint-plugin-smart'
 
-// 导出配置，使用eslint-plugin-smart的nestjs配置
 export default [
   ...eslintPlugin.configs.nestjs,
   ...eslintPlugin.configs.json,
-  {
-    // 为测试文件添加Jest环境配置
-    files: ['**/**.ts'],
-    languageOptions: {
-      globals: {
-        describe: true,
-        it: true,
-        expect: true,
-        beforeEach: true,
-        afterEach: true,
-        beforeAll: true,
-        afterAll: true,
-        jest: true,
-        process: true,
-        console: true,
-        module: true,
-        require: true,
-      },
-    },
-  },
 ]
