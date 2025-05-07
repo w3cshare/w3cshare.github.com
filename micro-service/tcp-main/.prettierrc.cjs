a073062e11aa700e91a4e2e37de575cbf445314d
wangwei <wwdqq7@qq.com>
2025-05-07 15:59:05 +0800

    ci(lint): 添加根目录 Lint 和格式化配置
    - 新增 .mono-eslint.mjs、.mono-ignore 和 .mono-prettier.mjs 文件
    - 更新 package.json 中的 Lint 相关命令
    - 修改 eslint-plugin-smart 和 eslint-plugin-smarts 的描述
    - 调整 eslint 规则推荐配置

--- micro-service/tcp-main/.prettierrc.cjs
+++ micro-service/tcp-main/.prettierrc.cjs
@@ -1,4 +1,11 @@
-
+/*
+ * @Author: wangwei wwdqq7@qq.com
+ * @Date: 2025-05-05 13:13:17
+ * @LastEditors: wangwei wwdqq7@qq.com
+ * @LastEditTime: 2025-05-07 15:56:51
+ * @FilePath: /FullStack/micro-service/tcp-main/.prettierrc.cjs
+ * @Description: prettierrc
+ */
 module.exports = {
   plugins: [require('prettier-plugin-smart')],
   ...require('prettier-plugin-smart').defaultOptions,
