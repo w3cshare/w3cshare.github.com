5c6babf13c1fa137d184146acdc10563dd5b18b3
wangwei <wwdqq7@qq.com>
2025-05-08 12:50:09 +0800

    ci: 添加 ESLint 配置并优化项目结构
    - 新增 .mono-stylelint.mjs 文件，配置 Stylelint 规则
    - 修改 lib-lint/stylelint-config-smart 和 lib-lint/stylelint-config-smarts 的 package.json 文件
    - 优化项目结构，统一代码风格

---
+++ .mono-stylelint.mjs
@@ -0,0 +1,14 @@
+/*
+ * @Author: wangwei wwdqq7@qq.com
+ * @Date: 2025-05-07 16:07:51
+ * @LastEditors: wangwei wwdqq7@qq.com
+ * @LastEditTime: 2025-05-08 12:48:17
+ * @FilePath: /FullStack/.mono-stylelint.mjs
+ * @Description:
+ */
+export default {
+  extends: ["stylelint-config-smart"],
+  rules: {
+    // 自定义覆盖规则（如需要）
+  },
+};
