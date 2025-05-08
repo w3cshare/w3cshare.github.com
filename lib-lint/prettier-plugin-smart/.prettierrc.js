500f186776d2d3db481ec066635797da76a5e292
wangwei <wwdqq7@qq.com>
2025-05-08 11:50:56 +0800

    build(lint): 更新 ESLint 和 Prettier 配置
    - 修改 ESLint 配置，使用本地路径导入 eslint-plugin-smart
    - 移除 eslint-plugin-smart 的 devDependencies
    - 修改 Prettier 配置，使用本地路径导入 prettier-plugin-smart
    - 移除 prettier-plugin-smart 的 devDependencies

--- lib-lint/prettier-plugin-smart/.prettierrc.js
+++ lib-lint/prettier-plugin-smart/.prettierrc.js
@@ -2,11 +2,11 @@
  * @Author: wangwei wwdqq7@qq.com
  * @Date: 2025-05-05 13:13:17
  * @LastEditors: wangwei wwdqq7@qq.com
- * @LastEditTime: 2025-05-06 15:29:04
+ * @LastEditTime: 2025-05-08 11:47:56
  * @FilePath: /FullStack/lib-lint/prettier-plugin-smart/.prettierrc.js
  * @Description:
  */
-import prettierPluginSmart from 'prettier-plugin-smart'
+import prettierPluginSmart from './lib/index.js'
 
 export default {
   plugins: [prettierPluginSmart],
--- lib-lint/prettier-plugin-smarts/.prettierrc.js
+++ lib-lint/prettier-plugin-smarts/.prettierrc.js
@@ -2,11 +2,11 @@
  * @Author: wangwei wwdqq7@qq.com
  * @Date: 2025-05-07 16:02:11
  * @LastEditors: wangwei wwdqq7@qq.com
- * @LastEditTime: 2025-05-07 17:06:06
+ * @LastEditTime: 2025-05-08 11:48:34
  * @FilePath: /FullStack/lib-lint/prettier-plugin-smarts/.prettierrc.js
  * @Description: prettier-plugin-smarts 配置文件
  */
-import prettierPluginSmart from 'prettier-plugin-smart'
+import prettierPluginSmart from './lib/index.js'
 
 export default {
   plugins: [prettierPluginSmart],
