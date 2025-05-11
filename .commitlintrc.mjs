7aa593692f29c178150c8c90d52c9f1c3e992859
wangwei <wwdqq7@qq.com>
2025-05-11 11:06:16 +0800

    ...

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -2,11 +2,18 @@
  * @Author: wangwei wwdqq7@qq.com
  * @Date: 2025-05-08 17:49:23
  * @LastEditors: wangwei wwdqq7@qq.com
- * @LastEditTime: 2025-05-10 23:51:11
+ * @LastEditTime: 2025-05-11 11:05:50
  * @FilePath: /FullStack/.commitlintrc.mjs
  * @Description:
  */
 import { defineConfig } from "commitlint-plugin-smart/cz-git";
+
+console.log(
+  defineConfig({
+    isMongo: true,
+  }),
+  123,
+);
 
 /** @type {import('cz-git').UserConfig} */
 export default defineConfig({
